9d2bf6f5ad5c4444103d5ea249189b33ab99cf38
Krisjanis Veinbahs <krisjanis.veinbahs@ui.com>
2025-11-13 19:59:48 +0200

    Refactor server to TS

---
+++ server/api/post/index.ts
@@ -0,0 +1,92 @@
+import Router from 'koa-router';
+import fetch from 'node-fetch';
+import URL from 'url-parse';
+import { JSDOM } from 'jsdom';
+import * as postParser from './postParser.js';
+import { Context, Next } from 'koa';
+
+const post = new Router();
+
+function isHostnameSS(hostname: string): boolean {
+    return !!hostname.match(/^(www\.)?ss\.(com|lv)$/);
+}
+
+function validateUrl(url: string): string {
+    const parsedUrl = new URL(url);
+
+    if (!isHostnameSS(parsedUrl.hostname)) {
+        throw new Error('Sludinājuma saitei kaut kas nav');
+    }
+    
+    return parsedUrl.toString().replace(/\/ru\//, '/lv/'); // Parse only LV ss.com (too lazy to implement both) :D
+}
+
+post.post('/post', async (ctx: Context, next: Next) => {
+    let url: string;
+    const body: any = ctx.request.body;
+
+    try {
+        try {
+            url = validateUrl(body.url);
+        } catch (err) {
+            url = validateUrl('https://' + body.url);
+        }
+    } catch (err: any) {
+        ctx.status = 500;
+        ctx.body = {
+            status: 'fail',
+            message: 'Sludinājuma saite neizskatās pareizi',
+            stack: err.stack?.split("\n").map((x: string) => x.trim())
+        };
+
+        return null;
+    }
+
+    await fetch(url)
+        .then((response: any) => {
+            if (response.status === 404) {
+                throw new Error('Sludinājums nav atrasts');
+            }
+
+            return response.text();
+        })
+        .then((response: string) => {
+            const ssdom = new JSDOM(response, {
+                contentType: "text/html"
+            });
+        
+            try {
+                const genericInfo = postParser.getPostGenericInfo(ssdom.window.document);
+                const addressInfo = postParser.getPostAddressInfo(ssdom.window.document);
+                const price = postParser.getPostPrice(ssdom.window.document);
+                const title = postParser.getPostTitle(ssdom.window.document);
+
+                ctx.body = {
+                    status: 'success',
+                    addressInfo,
+                    genericInfo,
+                    price,
+                    title,
+                    url
+                };
+            } catch (err: any) {
+                ctx.status = 500;
+                ctx.body = {
+                    status: 'fail',
+                    message: 'Neizdevās apstrādāt sludinājumu',
+                    stack: err.stack?.split("\n").map((x: string) => x.trim())
+                };
+            }
+        })
+        .catch((err: any) => {
+            ctx.status = 500;
+            ctx.body = {
+                status: 'fail',
+                message: 'Sludinājums nav atrasts',
+                stack: err.stack?.split("\n").map((x: string) => x.trim())
+            };
+        });
+});
+
+export default post;
+
---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import { Server } from 'http';
+import postRouter from './api/post/index.js';
+
+const app = new Koa();
+
+// Middleware - post requests
+app.use(bodyParser({
+    onerror: (err: Error, ctx: Koa.Context) => {
+        ctx.throw('Failed parsing request body.', 422);
+    }
+}));  
+
+// Register all API endpoints
+const routers = [postRouter];
+
+for (const router of routers) {
+    router.prefix('/api');
+    app.use(router.routes());
+}
+
+// Go live!
+const PORT = process.env.PORT || 3001;
+const server: Server = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+export default server;
+
