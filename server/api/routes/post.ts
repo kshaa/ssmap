05288be857af0f83807923a15c980dad7559663f
Krisjanis Veinbahs <krisjanis.veinbahs@ui.com>
2025-12-12 22:23:32 +0200

    Fix incorrect things

--- server/api/routes/post.ts
+++ server/api/routes/post.ts
@@ -6,7 +6,7 @@ const post = createRouter()
 
 post.post('/post', async (ctx) => {
   const body = await z.object({ url: z.string() }).parseAsync(ctx.request.body).catch((e) => { throw new ParseError({ entity: 'body', isUserError: true }, e) })
-  const post = (await ctx.services.ssSynchronizer.syncPost(body.url)).data
+  const post = (await ctx.services.ssSynchronizer.syncPost(body.url))
 
   ctx.status = 200
   ctx.body = post 
