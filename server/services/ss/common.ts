80525b0384ded58434bf82337470db0f67483cc4
Krisjanis Veinbahs <krisjanis.veinbahs@ui.com>
2025-12-22 21:51:24 +0200

    Reduce post TTL

--- server/services/ss/common.ts
+++ server/services/ss/common.ts
@@ -1,2 +1,2 @@
-export const MIN_POST_TTL_SECONDS = 24 * 60 * 60 // 24 hours is the minimum TTL for SS.LV posts
+export const MIN_POST_TTL_SECONDS = 30 * 60 // 30 minutes is the minimum TTL for SS.LV posts
 export const MIN_FEED_TTL_SECONDS = 5 // 5 seconds is the minimum TTL for SS.LV feeds
